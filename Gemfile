c63d1bb1d9321f1edfde1fcd4c5a97d37218f104
Adrien Bourgeois <adrien_bourgeois@iMac-de-Adrien.local>
2013-08-15 17:01:05 +0200

    Keyboard input for mail, password and delay between pokes

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'mechanize'
+gem 'highline'
 
 group :development do
   gem 'pry'
--- poker.rb
+++ poker.rb
@@ -1,6 +1,8 @@
+
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'bundler/setup'
+require 'highline/import'
 
 case ENV['RUBY_ENV']
   when 'production' then
@@ -13,8 +15,9 @@ class Poker
   attr_reader :agent
 
   def initialize(email, pass)
+    puts "a"
     @agent = Mechanize.new
-
+    puts "b"
     self.agent.get('http://m.facebook.com')
     puts agent.page.title
     self.login(email, pass)
@@ -34,24 +37,31 @@ class Poker
   end
 
   def poke_back_everybody
-    self.agent.get('http://m.facebook.com/pokes') #refresh the page
-    #get the names of the pokers:
+    self.agent.get('http://m.facebook.com/pokes')
     self.agent.page.search("[class='pokerName']").each do |link|
       puts "#{link.inner_text} poked back"
     end
 
-    #poke back:
     links = self.agent.page.links_with(text: 'Poke back')
     links.each{ |l| l.click }
   end
 
 end
 
-raise 'please provide ENV_EMAIL & ENV_PASS' unless ENV['ENV_EMAIL'] && ENV['ENV_PASS']
+print "Put your mail: "
+mail = gets.chomp
+#pass = gets.chomp
+pass = ask("Password: ") { |q| q.echo = "" }
 
-Poker.new(ENV['ENV_EMAIL'], ENV['ENV_PASS']).tap do |poker|
+print "Enter the minimum number of seconds between two pokes (has to be greater than 10): "
+delay = gets.chomp.to_i
+delay = 10 if delay == nil || delay < 10
+
+
+Poker.new(mail, pass).tap do |poker|
   while true do
     poker.poke_back_everybody
     sleep(5)
   end
 end
+
